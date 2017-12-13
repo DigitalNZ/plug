ba9e4f9aa77fb71080a8e3e31efb711000e955a8
Benedict Aluan <benedict@boost.co.nz>
2017-12-14 10:23:50 +1300

    Add helper to determine the enabled feature

--- lib/plug.rb
+++ lib/plug.rb
@@ -1,5 +1,13 @@
 require 'plug/engine'
 
 module Plug
-  # Your code goes here...
+  extend self
+
+  def enabled?(arg)
+    arg = arg.to_s if arg.is_a? Symbol
+    feature = Plug::Feature.slug_and_name(arg).first
+    
+    return false unless feature
+    return feature.enabled?
+  end
 end
---
+++ spec/lib/plug_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Plug do
+  describe '.enabled?' do
+    let!(:feature) { create(:feature, name: 'Map') }
+
+    context 'enabled' do
+      it 'returns true by slug' do
+        expect(Plug.enabled?('map')).to eq true
+      end
+      
+      it 'returns true by name' do
+        expect(Plug.enabled?('Map')).to eq true
+      end
+
+      it 'returns true by symbol' do
+        expect(Plug.enabled?(:map)).to eq true
+      end
+    end
+
+    context 'disabled' do
+      before { feature.disable! }
+
+      it 'returns false by slug' do
+        expect(Plug.enabled?('map')).to eq false
+      end
+      
+      it 'returns false by name' do
+        expect(Plug.enabled?('Map')).to eq false
+      end
+
+      it 'returns false by symbol' do
+        expect(Plug.enabled?(:map)).to eq false
+      end
+    end
+  end
+end
