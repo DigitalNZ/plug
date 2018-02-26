6c74b45528c19294636e8cd99d85b1f6548636ff
Ben <hapiben@users.noreply.github.com>
2018-02-26 13:03:38 +1300

    Enable HTTP Authentication (#19)

--- app/controllers/plug/application_controller.rb
+++ app/controllers/plug/application_controller.rb
@@ -2,8 +2,8 @@ module Plug
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    unless Rails.env.test?
-      # http_basic_authenticate_with name: Plug.auth_user, password: Plug.auth_password
+    if Plug.auth_user.present? && Plug.auth_password.present?
+      http_basic_authenticate_with name: Plug.auth_user, password: Plug.auth_password
     end
   end
 end
