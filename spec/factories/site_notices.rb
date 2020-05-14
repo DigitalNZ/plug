51b25c83bd1c16e721136750336bbd3da6c5b494
Ben Aluan <benedict@boost.co.nz>
2020-05-15 11:29:40 +1200

    Update factory files syntax

--- spec/factories/site_notices.rb
+++ spec/factories/site_notices.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :site_notice, class: '::Plug::SiteNotice' do
-    name        'Awesome Site Notice'
-    notice      'This is a notice'
-    theme       'background:#CCC;color:#000;icon:info-circle'
+    name    { 'Awesome Site Notice' }
+    notice  { 'This is a notice' }
+    theme   { 'background:#CCC;color:#000;icon:info-circle' }
 
     trait :invalid do
-      name ''
+      name { '' }
     end
 
     trait :default do
-      theme 'background:#9D201F;color:#F2F2F2;icon:flag'
+      theme { 'background:#9D201F;color:#F2F2F2;icon:flag' }
     end
   end
 end
