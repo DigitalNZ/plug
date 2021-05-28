461e2026f362e8c8b445d9c92cc7e0149ff7387d
Ben Aluan <benedict@boost.co.nz>
2021-05-28 14:32:33 +1200

    chore(updates): Fix rubocop offenses

--- app/controllers/plug/features_controller.rb
+++ app/controllers/plug/features_controller.rb
@@ -65,19 +65,19 @@ module Plug
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_feature
-        @feature = Feature.find(params[:id])
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_feature
+      @feature = Feature.find(params[:id])
+    end
 
-      # Only allow a trusted parameter "white list" through.
-      # TODO: Strong params not available for older Rails
-      def feature_params
-        if Rails.version.to_i < 5
-          ActiveSupport::HashWithIndifferentAccess.new(params[:feature])
-        else
-          params.require(:feature).permit(:name, :description, :state, :notice)
-        end
+    # Only allow a trusted parameter "white list" through.
+    # TODO: Strong params not available for older Rails
+    def feature_params
+      if Rails.version.to_i < 5
+        ActiveSupport::HashWithIndifferentAccess.new(params[:feature])
+      else
+        params.require(:feature).permit(:name, :description, :state, :notice)
       end
+    end
   end
 end
